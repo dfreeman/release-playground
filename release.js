20918cde1d6561981573c6c4a319725308e40faa
Dan Freeman <dfreeman@salsify.com>
2020-02-11 15:46:15 -0700

    All-in-one, I guess

--- release.js
+++ release.js
@@ -7,24 +7,22 @@ const Octokit = require('@octokit/rest');
 
 (async () => {
   const github = new Octokit({ auth: process.env.GITHUB_TOKEN });
+  const tag = process.env.RELEASE_TAG;
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
   const releases = await github.repos.listReleases({ owner, repo });
 
   const release = releases.data.find(release => release.draft);
   if (release) {
-    console.log({ owner, repo });
     await github.repos.updateRelease({
       owner,
       repo,
       release_id: release.id,
-      tag_name: Math.random().toString(36).slice(2)
+      tag_name: tag,
+      draft: false,
     });
-    console.log('updated release', release);
   } else {
-    console.log('no unpublished release found');
+    core.setFailed('No unpublished release found');
   }
-
-  core.setOutput('result', 'all good');
 })().catch(e => {
   core.setFailed(e.message);
 });
